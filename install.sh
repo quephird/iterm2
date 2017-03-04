b934f98d90b43d390f1996dd7d2f6a79943e699c
danielle kefford <danielle@danielles-MacBook-Pro.local>
2017-03-04 16:41:06 -0500

    Script now installs images to .iterm2, scripts to ~/Library/Scripts, and better handles iTerm2 preferences.

--- install.sh
+++ install.sh
@@ -1,12 +1,26 @@
-echo Creating preferences file for current user...
+#! /bin/sh
+
+echo Creating new preferences file for current user...
 sed -e "s|{HOME_DIRECTORY}|$HOME|g" iterm_profile_template.xml > com.googlecode.iterm2.plist
 
-echo Copying scripts and images directories to $HOME/iterm2...
-mkdir -p ~/iterm2
-mkdir -p ~/iterm2/scripts
-mkdir -p ~/iterm2/images
-cp -r scripts ~/iterm2/
-cp -r images ~/iterm2/
+echo Copying images directory to $HOME/.iterm2...
+mkdir -p ~/.iterm2
+mkdir -p ~/.iterm2/images
+cp -r images ~/.iterm2/
+
+echo Copying AppleScripts to $HOME/Library/Scripts...
+if [ ! -d "~/Library/Scripts" ]; then
+  mkdir -p ~/Library/Scripts
+fi
+cp -r scripts/* ~/Library/Scripts
+
+echo Deleting cached preferences...
+defaults delete com.googlecode.iterm2
 
 echo Moving preferences file to ~/Library/Preferences/...
 mv com.googlecode.iterm2.plist ~/Library/Preferences/
+
+echo Reading in new preferences...
+defaults read com.googlecode.iterm2 > /dev/null
+
+echo Done!!!
